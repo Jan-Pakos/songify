b616a0ccd10f7af79682592ce70624120f53d5be
Jan-Pakos <jan.p.pakos@gmail.com>
2025-10-28 20:00:21 +0100

    uopdate

--- src/main/java/com/songify/song/infrastructure/controller/dto/response/GetAllSongsResponseDto.java
+++
@@ -1,7 +0,0 @@
-package com.songify.song.infrastructure.controller.dto.response;
-
-import com.songify.song.domain.model.Song;
-import java.util.Map;
-
-public record GetAllSongsResponseDto(Map<Integer, Song> songs) {
-}

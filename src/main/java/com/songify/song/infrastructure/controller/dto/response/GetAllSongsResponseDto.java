9f4712e3e5de433b2ca1561b3ed41efbb695cec0
Jan-Pakos <jan.p.pakos@gmail.com>
2025-11-08 19:35:18 +0100

    change CRUDrepo to Repository

--- src/main/java/com/songify/song/infrastructure/controller/dto/response/GetAllSongsResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.songify.song.infrastructure.controller.dto.response;
-
-import com.songify.song.domain.model.Song;
-
-import java.util.List;
-import java.util.Map;
-
-public record GetAllSongsResponseDto(List<SongDto> songs) {
-}

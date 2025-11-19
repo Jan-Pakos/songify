0a98a68d4398a4d29201329a58e4bd41ec990757
Jan-Pakos <janpakos123@gmail.com>
2025-11-19 18:42:34 +0100

    added feature POST artist

---
+++ target/classes/db.migration/V12__insert_albums.sql
@@ -0,0 +1,11 @@
+INSERT INTO album (title, release_date) VALUES
+                                            ('The Dark Side of the Moon', '1973-03-01 00:00:00+00'),
+                                            ('Abbey Road', '1969-09-26 00:00:00+00'),
+                                            ('Thriller', '1982-11-30 00:00:00+00'),
+                                            ('Back in Black', '1980-07-25 00:00:00+00'),
+                                            ('Rumours', '1977-02-04 00:00:00+00'),
+                                            ('Hotel California', '1976-12-08 00:00:00+00'),
+                                            ('Nevermind', '1991-09-24 00:00:00+00'),
+                                            ('Born to Run', '1975-08-25 00:00:00+00'),
+                                            ('OK Computer', '1997-05-21 00:00:00+00'),
+                                            ('Led Zeppelin IV', '1971-11-08 00:00:00+00');
---
+++ target/classes/db.migration/V16__insert_songs.sql
@@ -0,0 +1,13 @@
+INSERT INTO song (name, release_date, duration, uuid) VALUES
+                                                          ('Time', '1973-03-01 00:00:00+00', 412, gen_random_uuid()),
+                                                          ('Come Together', '1969-09-26 00:00:00+00', 259, gen_random_uuid()),
+                                                          ('Billie Jean', '1982-11-30 00:00:00+00', 294, gen_random_uuid()),
+                                                          ('You Shook Me All Night Long', '1980-07-25 00:00:00+00', 210, gen_random_uuid()),
+                                                          ('Dreams', '1977-02-04 00:00:00+00', 257, gen_random_uuid()),
+                                                          ('Hotel California', '1976-12-08 00:00:00+00', 391, gen_random_uuid()),
+                                                          ('Smells Like Teen Spirit', '1991-09-24 00:00:00+00', 301, gen_random_uuid()),
+                                                          ('Born to Run', '1975-08-25 00:00:00+00', 279, gen_random_uuid()),
+                                                          ('Paranoid Android', '1997-05-21 00:00:00+00', 387, gen_random_uuid()),
+                                                          ('Stairway to Heaven', '1971-11-08 00:00:00+00', 482, gen_random_uuid());
+
+

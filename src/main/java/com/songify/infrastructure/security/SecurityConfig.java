03adf8a9895c5c44b5c8922278002ea7921dbfde
Jan-Pakos <janpakos123@gmail.com>
2025-12-07 15:04:43 +0100

    added role based access to endpoints

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.songify.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -31,9 +33,23 @@ class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(c -> c.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/swagger-resources/**").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
+                .requestMatchers("/users/register/**").permitAll()
+                .requestMatchers(HttpMethod.GET,"/songs/**").permitAll()
+                .requestMatchers(HttpMethod.GET,"/artists/**").permitAll()
+                .requestMatchers(HttpMethod.GET,"/albums/**").permitAll()
+                .requestMatchers(HttpMethod.GET,"/genres/**").permitAll()
+                .requestMatchers(HttpMethod.POST,"/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PATCH,"/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE,"/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT,"/songs/**").hasRole("ADMIN")
+                .anyRequest().authenticated());
         return http.build();
     }
 

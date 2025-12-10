842e7a92c03430cc28e75d9d70f17e51f68da548
Jan-Pakos <janpakos123@gmail.com>
2025-12-10 11:39:26 +0100

    preparing an empty filter and adding it to the chain

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.songify.infrastructure.security;
 
+import com.songify.infrastructure.security.jwt.JwtAuthTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -44,11 +47,12 @@ class SecurityConfig {
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthTokenFilter jwtAuthTokenFilter) throws Exception {
         http.formLogin(c -> c.disable());
         http.httpBasic(c -> c.disable());
         http.csrf(c -> c.disable());
         http.sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(jwtAuthTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         http.cors(corsConfigurerCustomizer());
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
--- src/main/java/com/songify/infrastructure/security/jwt/JwtTokenController.java
+++ src/main/java/com/songify/infrastructure/security/jwt/JwtTokenController.java
@@ -6,14 +6,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.NoSuchAlgorithmException;
+
 @RestController
 @RequiredArgsConstructor
-class JwtTokenController {
+public class JwtTokenController {
 
     private final JwtTokenGenerator jwtTokenGenerator;
 
     @PostMapping("/token")
-    public ResponseEntity<JwtResponseDto> authenticateAndGenerateToken(@RequestBody TokenRequestDto dto) {
+    public ResponseEntity<JwtResponseDto> authenticateAndGenerateToken(@RequestBody TokenRequestDto dto) throws NoSuchAlgorithmException {
         String token = jwtTokenGenerator.authenticateAndGenerateToken(dto.username(), dto.password());
         return ResponseEntity.ok(
                 JwtResponseDto.builder()
